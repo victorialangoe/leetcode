4a3d788cb0cacfa1d79550666b6403b29c48636c
victorialangoe <89309733+victorialangoe@users.noreply.github.com>
2023-03-15 13:55:27 +0100

    Added an easy task

---
+++ easy/AddTwoNumbers.java
@@ -0,0 +1,20 @@
+// from https://leetcode.com/problems/two-sum/
+
+class AddTwoNumbers {
+    public int[] twoSum(int[] nums, int target) {
+        int num[] = new int[2];
+        for (int i = 0; i < nums.length; i++){
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] + nums[j] == target){
+                    num[0] = i;
+                    num[1] = j;
+                    
+                    
+                }
+            }
+        }
+
+        return num;
+        
+    }
+}
