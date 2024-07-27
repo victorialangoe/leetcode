8cfbd53655099321b31d810408dba75be0326be4
Victoria Ovedie Chruickshank Langø <victorialangoe@icloud.com>
2024-07-27 14:36:54 +0200

    new easy problem

---
+++ easy/MergeSortedArray.py
@@ -0,0 +1,25 @@
+from typing import List
+
+class Solution:
+    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+        """
+        Do not return anything, modify nums1 in-place instead.
+        """
+        i = m+n-1
+        j = m-1
+        k = n-1
+
+        while j >=0 and k >=0:
+            if nums1[j]<=nums2[k]:
+                nums1[i]=nums2[k]
+                k = k-1
+            else:
+                nums1[i]=nums1[j]
+                j = j-1
+            i = i-1
+
+        while k >= 0:
+            nums1[i] = nums2[k]
+            k = k-1
+            i = i-1
+                    
